ce2adc5f7dbc4d6ad9ce4a4d6b70187dd6056f8d
zzr1226sdo <yc117602@hotmail.com>
2022-08-02 14:53:58 +0800

    page1

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/jquery-1" => "pages#jquery_1"
+  get "/jquery-2" => "pages#jquery_2"
+  get "/jquery-3" => "pages#jquery_3"
+  get "/jquery-4" => "pages#jquery_4"
+  get "/jquery-5" => "pages#jquery_5"
+
+  root "pages#jquery_1"
+
+end
